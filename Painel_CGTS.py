e7a3db24d4dfea76c20324cf130660f1a55b39fe
Thais Salzer Procopio <106846359+thaissalzer@users.noreply.github.com>
2024-02-06 12:02:10 -0300

    Update Painel_CGTS.py

--- Painel_CGTS.py
+++ Painel_CGTS.py
@@ -7,8 +7,6 @@ from selenium import webdriver
 import difflib
 from chromedriver_autoinstaller import install as install_chromedriver
 
-install_chromedriver()
-
 
 st.set_page_config(layout = 'wide')
 #inserindo o titulo
