bfc0bfe510bf2f0998f6e072ec194e283515f565
Thais Salzer Procopio <106846359+thaissalzer@users.noreply.github.com>
2024-02-11 16:08:00 -0300

    Update Painel_CGTS.py

--- Painel_CGTS.py
+++ Painel_CGTS.py
@@ -167,10 +167,10 @@ def get_chromedriver_path():
 
 def get_webdriver_options():
     options = Options()
-    options.add_argument("--headless")
+    options.add_argument("--headless=new")
+    options.add_argument('--disable-gpu')
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--disable-gpu")
     options.add_argument("--disable-features=NetworkService")
     options.add_argument("--window-size=1920x1080")
     options.add_argument("--disable-features=VizDisplayCompositor")
